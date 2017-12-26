54315f51cded17f5c9fb8cd389b6757bc9c41d27
Mark <mark.cos@ntlworld.com>
2017-12-26 00:06:24 +0000

    S04L125 - Creating an AI Controller Class
    This AI controller will poccess any tank objects in the scene that aren't controlled by the TankPlayerController. Create a C++ AIController class called TankAIController. Then open Tank_BP and in Class Defaults in the Details - Pawn section change the AI Controller Class from AIController to TankAIController. Whereas before, we created a blueprint first and then the C++ class (not forgetting to set the parent of the blueprint to the C++ class afterwards) this time it's the other way round.
    Copy the code from the TankPlayerController.h and .cpp (change some references from ATankPlayerController to ATankAIController) and log that the BeginPlay() routine is logging AI tanks.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI not possession of a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI in possession of tank: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	/*
+	Because we need to access members of ATank, without the cast you wouldn't be able to call any of the functions of ATank.
+	*/
+	return Cast<ATank>(GetPawn());
+}
