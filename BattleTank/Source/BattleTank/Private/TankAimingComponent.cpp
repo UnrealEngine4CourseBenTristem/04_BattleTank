02b1b725d2eba3aedfd99555dae2ff3ac846eeec
Mark <mark.cos@ntlworld.com>
2017-12-27 21:15:00 +0000

    S04L136 - Predict Projectile Landing Point
    Created a socket on the end of the barrel in Barrel.fbx mesh file. The socket was named BarrelFiringPoint.
    
    Use this socket as the StartLocation for the UGameplayStatics::SuggestProjectileVelocity function
    
    Use the UGameplayStatics::SuggestProjectileVelocity(..) function in the AimAt() function of TankAimingComponent.cpp

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #include "TankAimingComponent.h"
 #include "Engine.h"
+#include "GameFramework/Gamemode.h"
 
 // Sets default values for this component's properties
 UTankAimingComponent::UTankAimingComponent()
@@ -45,11 +47,47 @@ void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, F
 
 void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 {
-	//auto OurTankName = GetOwner()->GetName(); // GetOwner causes intellisense error if Engine.h is not included at the top of this file
+	auto OurTankName = GetOwner()->GetName(); // GetOwner causes intellisense error if Engine.h is not included at the top of this file
 	//auto BarrelLocation = Barrel->GetComponentLocation().ToString() ;
 	//UE_LOG(LogTemp, Warning, TEXT("Tank.cpp :- %s aiming at location : %s  : from barrel location %s "), *OurTankName, *HitLocation.ToString(), *BarrelLocation);
 
-	UE_LOG(LogTemp, Warning, TEXT("Launchspeed %f "), LaunchSpeed);
+
+	// protect the Barrel 
+	if (!Barrel) { return; }
+
+	// WorldContextObject will be the keyword 'this'
+
+	// Toss Velocity
+	FVector OutLaunchVelocity;
+
+	// Start Location
+	FVector StartLocation = Barrel->GetSocketLocation(FName("BarrelFiringPoint"));
+
+	// Calculate the OutLaunchVelocity
+	if (	UGameplayStatics::SuggestProjectileVelocity(
+				this,
+				OutLaunchVelocity,
+				StartLocation,
+				HitLocation,
+				LaunchSpeed,
+				false,
+				0,
+				0,
+				ESuggestProjVelocityTraceOption::DoNotTrace
+
+			)
+		)
+	{
+
+	
+		// Aim direction
+		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
+
+
+		UE_LOG(LogTemp, Warning, TEXT("%s aiming at  %s "), *OurTankName, *AimDirection.ToString());
+
+	}
+
 
 }
 
